2282871a762c124cd109cfddad04d6b53ce85c01
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-11-08 17:46:27 -0500

    fix: Replace "stringKey" with "key" to fix "invalid argument" error
    Replace "stringKey" with "key" inside "getCustomer()" function to fix "invalid argument" error

--- src/main.go
+++ src/main.go
@@ -252,7 +252,7 @@ func getCustomer(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.WriteHeader(http.StatusAccepted)
 
-	json.NewEncoder(w).Encode(customerMapsForAPI[stringKey])
+	json.NewEncoder(w).Encode(customerMapsForAPI[key])
 }
 
 func addCustomer(w http.ResponseWriter, r *http.Request) {
